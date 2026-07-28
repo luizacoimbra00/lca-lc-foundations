b76fa7dfe84a3b971de5a54ac47d33341e699495
Luiza Coimbra <luiza.alfonso@pucrs.br>
2026-07-28 14:44:02 -0300

    memory

--- notebooks/module-1/memory.py
+++ notebooks/module-1/memory.py
@@ -7,6 +7,48 @@
 #especialmente quando os agentes se tornam mais complexos e mantêm muito mais do que apenas o histórico de mensagens.
 
 from langgraph.checkpoint.memory import InMemorySaver  
+import os
+from pprint import pprint
+from langchain.agents import create_agent
+from langchain.messages import HumanMessage
+from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
 
 load_dotenv()
+
+def agente_com_memoria():
+
+    model = ChatOllama(
+        model="qwen2.5:7b",
+        base_url=os.getenv("OLLAMA_API_KEY")
+    )
+
+    agent = create_agent(
+        model=model,
+        checkpointer=InMemorySaver(), #inicializa o agente com o checkpointer: "Sempre que houver uma conversa, salve o estado dela na memória."    
+    )
+
+    question = HumanMessage(content="Hello my name is Seán and my favourite colour is green")
+    
+    config = {"configurable": {"thread_id": "1"}} #config tbm é um dicionário, mas é um dicionário dentro de outro. 
+    #dicionário externo --> chave: configurable / valor: {"thread_id": "1"}. Este valor também é um dicionário. 
+    #dicionário interno --> chave: thread_id / valor: 1. Esse valor 1 representa um chat, uma conversa que deve ser memorizada. cada thread id é um histórico/memória diferente. 
+
+    response = agent.invoke( #ao chamar o agente, o invoke recebe dois argumentos: o estado de entrada (mensagem do usuário)
+    #e a configuração de execução, que diz que a mensagem pertence à conversa cujo thread_id é 1
+        {"messages": [question]},
+        config,  
+    )
+    
+    question2 = HumanMessage(content="What's my favourite colour?")
+    
+    response2 = agent.invoke(
+    {"messages": [question2]},
+    config, #diz que a question2 pertence à mesma configuração que a question (thread_id 1), assim, a response2 vai ter o histórico de todas as invokes relacionadas à thread_id 1.
+    )
+    
+    for msg in response2["messages"]:
+        print(f"{msg.type}: {msg.content}") #atributo content está dentro dos objetos HumanMessage ou AiMessage e se refere apenas as mensagens em si, sem os metadados etc
+
+if __name__ == "__main__":
+    agente_com_memoria()
